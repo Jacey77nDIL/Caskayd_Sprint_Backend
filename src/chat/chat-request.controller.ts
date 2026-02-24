05ce6859dde1ca298089f6a8a8f92c2fe7232163
Ojo Jeremiah <ojojeremiah249@gmail.com>
2026-02-24 12:28:55 +0100

    Day 4, I guess.

--- src/chat/chat-request.controller.ts
+++ src/chat/chat-request.controller.ts
@@ -1,31 +1,35 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post, Body, Get, Req, Patch, Param, UseGuards } from "@nestjs/common";
 import { ChatRequestService } from "./chat-request.service";
-import { Post,Body, Get, Req, Patch, Param } from "@nestjs/common";
 import { CreateChatRequestDto } from "./dto/create-chat-request.dto";
-import { UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Controller("chat-requests")
 export class ChatRequestController {
   constructor(private service: ChatRequestService) {}
 
+  // Business sends request
   @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() dto: CreateChatRequestDto, @Req() req) {
     return this.service.create(dto, req.user.sub);
   }
 
+  // Creator views incoming requests
+  @UseGuards(AuthGuard('jwt'))
   @Get("creator")
   getForCreator(@Req() req) {
-    return this.service.findForCreator(req.user.id);
+    return this.service.findForCreator(req.user.sub);
   }
 
+  // Creator accepts request
+  @UseGuards(AuthGuard('jwt'))
   @Patch(":id/accept")
   accept(@Param("id") id: string) {
-    console.log('ACCEPT REQUEST:', id);
     return this.service.accept(id);
   }
 
+  // Creator rejects request
+  @UseGuards(AuthGuard('jwt'))
   @Patch(":id/reject")
   reject(@Param("id") id: string) {
     return this.service.reject(id);
